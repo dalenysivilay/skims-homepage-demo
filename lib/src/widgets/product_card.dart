ab67bf66ce3988dfd3e274271ded9eafeaa07af9
Daleny Sivilay <hi@daleny.dev>
2023-12-09 12:36:00 -0600

    View product modal complete

--- lib/src/widgets/product_card.dart
+++ lib/src/widgets/product_card.dart
@@ -15,7 +15,9 @@ class ProductCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {},
+      onTap: () {
+        _dialogBuilder(context, image, name, price);
+      },
       child: Column(
         children: [
           SizedBox(
@@ -44,4 +46,63 @@ class ProductCard extends StatelessWidget {
       ),
     );
   }
+}
+
+Future<void> _dialogBuilder(
+    BuildContext context, String image, String name, int price) {
+  return showDialog<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Center(
+          child: Text(
+            'View Product'.toUpperCase(),
+            style: const TextStyle(
+              color: Color(0xFF625546),
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        backgroundColor: Colors.white,
+        content: Container(
+          height: 350,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+          ),
+          child: ProductCard(
+            image: image,
+            name: name,
+            price: price,
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.arrow_back_ios_outlined,
+                color: Color(0xFF625546),
+                size: 24,
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.add_shopping_cart_outlined,
+                color: Color(0xFF625546),
+                size: 24,
+              ),
+            ),
+          ),
+        ],
+        actionsAlignment: MainAxisAlignment.spaceBetween,
+      );
+    },
+  );
 }
